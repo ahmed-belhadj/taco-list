bbf0083d8cbcf38b109149527e25b35eba6973b1
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-08 22:40:47 -0500

    handleAddTaco() and renderTacoList()

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 const addTaco = document.querySelector(".add-taco");
 const tacoList = document.querySelector(".tacos");
 const tacos = [];
+
+function handleAddTaco(event) {
+  event.preventDefault();
+  const text = this.querySelector("[name=taco]").value;
+  const taco = {
+    text,
+    done: false
+  };
+  tacos.push(taco);
+  renderTacoList(tacos, tacoList);
+  this.reset();
+}
+function renderTacoList(items = [], itemList) {
+  tacoList.innerHTML = items
+    .map((item, i) => {
+      return `<li><label for="">${item.text}</label></li>`;
+    })
+    .join("");
+}
+addTaco.addEventListener("submit", handleAddTaco);
